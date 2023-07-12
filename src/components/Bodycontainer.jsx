9bd76dc7d84f9eb34497237552b97e91dfe5546c
Uday <udaybharadwa2002@gmail.com>
2023-07-13 00:59:08 +0530

    Base Created

---
+++ src/components/Bodycontainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainContainer from './MainContainer'
+export const BodyContainer = () => {
+  return (
+    <div>
+        <MainContainer/>
+    </div>
+  )
+}
+
+export default BodyContainer
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const MainContainer = () => {
+  return (
+    <div>
+        <Header/>
+        MainContainer
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainContainer
